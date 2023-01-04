6d45216daccb9bc45c6b82f4816f5b124cc8c172
Björn Hedqvist <bjornhedqvist22@gmail.com>
2023-01-04 13:19:34 -0800

    completed setup of .env and connection to database

--- src/db/migrations/20230104122152_createReviews.js
+++ src/db/migrations/20230104122152_createReviews.js
@@ -1,12 +1,12 @@
 
 exports.up = function(knex) {
     return knex.schema.createTable("reviews", (table) => {
-        table.increments("review_id").primary() // Sets review_id as the primary key
-        table.string("content")
-        table.integer("score")
-        table.foreign("critic_id").references("critic_id").inTable("critics").onDelete("CASCADE")
-        table.foreign("movie_id").references("movie_id").inTable("movies").onDelete("CASCADE")
-        table.timestamps(true, true) // Adds created_at and updated_at fields
+        table.increments("review_id").primary(); // Sets review_id as the primary key
+        table.string("content");
+        table.integer("score");
+        table.foreign("critic_id").references("critic_id").inTable("critics").onDelete("CASCADE");
+        table.foreign("movie_id").references("movie_id").inTable("movies").onDelete("CASCADE");
+        table.timestamps(true, true); // Adds created_at and updated_at fields
       });
 };
 
