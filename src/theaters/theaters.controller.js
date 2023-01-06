0a44965a0a7003ff587095c6b4e4cbded94e617f
Björn Hedqvist <bjornhedqvist22@gmail.com>
2023-01-05 20:38:53 -0800

    further work on get /theaters route
    error persisting, reworking the reduce-properties utility to receive the right data format from the get request

--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -1,12 +1,23 @@
 const service = require("./theaters.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
-
+const reduceProperties = require("../utils/reduce-properties");
 
 async function list(req, res) {
-  await service.list()
-  const data = await service.reduceWithMovies()
-  console.log(data)
-  res.json({ data })
+  const data = await service.list()
+  const config = {
+            movie_id: ["movies", null, "movie_id"],
+            title: ["movies", null, "title"],
+            runtime_in_minutes: ["movies", null, "runtime_in_minutes"],
+            rating: ["movies", null, "rating"],
+            description: ["movies", null, "description"],
+            image_url: ["movies", null, "image_url"],
+            created_at: ["movies", null, "created_at"],
+            updated_at: ["movies", null, "updated_at"],
+            is_showing: ["movies", null, "is_showing"],
+            theater_id: ["movies", null, "theater_id"]
+    }
+  const reduceMovies = reduceProperties("theater_id", config)
+  res.json({ data: reduceMovies(data) })
 }
 
 module.exports = {
--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,5 +1,4 @@
 const knex = require("../db/connection");
-const reduceProperties = require("../utils/reduce-properties");
 
 async function list() {
   return knex("theaters as t")
@@ -9,18 +8,18 @@ async function list() {
     .groupBy("t.theater_id");
 }
 
-const addMovies = reduceProperties("movie_id", {
-        movie_id: ["movies", null, "movie_id"],
-        title: ["movies", null, "title"],
-        runtime_in_minutes: ["movies", null, "runtime_in_minutes"],
-        rating: ["movies", null, "rating"],
-        description: ["movies", null, "description"],
-        image_url: ["movies", null, "image_url"],
-        created_at: ["movies", null, "created_at"],
-        updated_at: ["movies", null, "updated_at"],
-        is_showing: ["movies", null, "is_showing"],
-        theater_id: ["movies", null, "theater_id"]
-})
+// const addMovies = reduceProperties("theater_id", {
+//         movie_id: ["movies", null, "movie_id"],
+//         title: ["movies", null, "title"],
+//         runtime_in_minutes: ["movies", null, "runtime_in_minutes"],
+//         rating: ["movies", null, "rating"],
+//         description: ["movies", null, "description"],
+//         image_url: ["movies", null, "image_url"],
+//         created_at: ["movies", null, "created_at"],
+//         updated_at: ["movies", null, "updated_at"],
+//         is_showing: ["movies", null, "is_showing"],
+//         theater_id: ["movies", null, "theater_id"]
+// })
 
 async function reduceWithMovies(){
   return knex("movies as m")
